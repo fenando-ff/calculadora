8cfc4932f30802bc78fe516c39f21e3e397dd120
fenando-ff <fernandofreitassud2016@gmail.com>
2024-08-27 16:57:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ function calcular() {
 
     var n1 = parseFloat(document.getElementById('n1').value);
     var n2 = parseFloat(document.getElementById('n2').value);
-    var oper = document.getElementById('operacao').value;
+    var operacao = document.getElementById('operacao').value;
     var resultado;
 
-    switch (oper) {
+    switch (operacao) {
         case "soma":
             resultado = n1 + n2
             break;
