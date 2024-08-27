4af6713489be5dbcf4ebc8b261b74614846f6065
fenando-ff <fernandofreitassud2016@gmail.com>
2024-08-27 14:03:34 -0300

    add

---
+++ main.js
@@ -0,0 +1,31 @@
+
+function calcular() {
+
+    var n1 = parseFloat(document.getElementById('n1').value);
+    var n2 = parseFloat(document.getElementById('n2').value);
+    var oper = document.getElementById('operacao').value;
+    var resultado;
+
+    switch (oper) {
+        case "soma":
+            resultado = n1 + n2
+            break;
+        case "mult":
+            resultado = n1 * n2
+            break;
+        case 'sub':
+            resultado = n1 - n2;
+            break;
+        case 'divi':
+            if (n2 === 0) {
+                document.getElementById("resultado").innerHTML = "Divisão por zero não é permitida";
+                return;
+            }
+            resultado = n1 / n2;
+            break;
+        default:
+            resultado = "inválida";
+    }
+
+    document.getElementById('resultado').innerHTML = `O resultado da operação é ${resultado}`
+}
