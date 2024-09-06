437ec85224812788c58819f207b645353f9c29f3
fenando-ff <fernandofreitassud2016@gmail.com>
2024-09-06 14:38:40 -0300

    Update main.js
    botão de limpar funcionando

--- main.js
+++ main.js
@@ -29,3 +29,8 @@ function calcular() {
 
     document.getElementById('resultado').innerHTML = `O resultado da operação é ${resultado}`
 }
+
+function limpar() {
+    var n1 = parseFloat(document.getElementById('n1').value ="");
+    var n2 = parseFloat(document.getElementById('n2').value ="");
+}
